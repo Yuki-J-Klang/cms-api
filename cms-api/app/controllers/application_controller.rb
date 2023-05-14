c6e745f2c347f07d16733d10c7de7ad501390a82
Yuki-J-Klang <Yuki.i.000915@gmail.com>
2023-05-14 22:33:00 +0900

    エラーハンドリング追加（途中）

--- cms-api/app/controllers/application_controller.rb
+++ cms-api/app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::API
+  def users_success(users)
+    render status: 200, json: {
+      data: users
+    }
+  end
+
+  def user_success(user)
+    render status: 200, json: {
+      id: user.id,
+      email: user.email,
+      password: user.password
+    }
+  end
+
+
+  def render_400
+    render status: 400, json: {
+      message: BadRequest
+    }
+  end
+
+  def render_404
+    render status: 404, json: {
+      message: SessionError
+    }
+  end
+
+  def render_500
+    render status: 500, json: {
+      message: InternalServerError 
+    }
+  end
+
 end
