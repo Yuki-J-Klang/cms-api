2045f8c18fb9797985a60980d6a9c7c9e304414b
Yuki-J-K-lang <Yuki.i.000915@gmail.com>
2023-05-30 16:33:52 +0900

    delete: db/timestamp

--- cms-api/db/schema.rb
+++ cms-api/db/schema.rb
@@ -14,8 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_052000) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email"
     t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
